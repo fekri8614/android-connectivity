89878d776705818b25a910bc5d346f75ba6c575e
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-07-27 01:41:46 -0700

    added jetpack compose

--- app/src/main/java/info/fekri/connsamp/MainActivity.kt
+++ app/src/main/java/info/fekri/connsamp/MainActivity.kt
@@ -3,18 +3,13 @@ package info.fekri.connsamp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import info.fekri.connsamp.databinding.ActivityMainBinding
 import info.fekri.connsamp.ui.theme.BackgroundMain
 import info.fekri.connsamp.ui.theme.ConnSampTheme
-import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectIndexed
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
